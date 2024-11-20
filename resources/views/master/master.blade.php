aeec3612a37f641ad1432aee3e17d74573842461
MarioOrlando82 <mario796gt@gmail.com>
2024-11-20 13:55:01 +0700

    feat: add alert session

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -19,6 +19,12 @@
 
 <body>
     @include('components.navbar')
+    @if (session('error'))
+    <div class="alert alert-danger mt-3">{{ session('error') }}</div>
+    @endif
+    @if (session('success'))
+        <div class="alert alert-success mt-3">{{ session('success') }}</div>
+    @endif
     @yield('content')
     @include('components.footer')
     <script src="{{asset('bootstrap/js/bootstrap.bundle.js')}}"></script>
