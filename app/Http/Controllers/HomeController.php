05abe0cfeb99b19625f0cacdb615708095588aa3
MarioOrlando82 <mario796gt@gmail.com>
2024-11-20 14:29:35 +0700

    feat: add filter and sorting

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,11 +7,31 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $books = Book::paginate(10);
+        $category = $request->get('category');
+        $sort = $request->get('sort');
+
+        $query = Book::query();
+
+        if ($category && $category !== 'all') {
+            $query->whereHas('category', function ($query) use ($category) {
+                $query->where('name', $category);
+            });
+        }
+
+        if ($sort == 'price_asc') {
+            $query->orderBy('price', 'asc');
+        } elseif ($sort == 'price_desc') {
+            $query->orderBy('price', 'desc');
+        }
+
+        $books = $query->paginate(12);
+
         return view('homepage', compact('books'));
     }
+
+
 
     public function show($id)
     {
