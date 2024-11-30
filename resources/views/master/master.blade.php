9d1df0cbe679ef519039250f7425db7b7b223a2a
MarioOrlando82 <mario796gt@gmail.com>
2024-11-30 09:08:36 +0700

    feat: test deploy

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>Bukulapak</title>
     <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
     <style>
