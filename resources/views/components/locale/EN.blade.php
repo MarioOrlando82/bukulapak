da0df92bed51e0e9042815336ea8e5403f5ec3fa
StephenEpen <stephentio.23@gmail.com>
2024-12-14 11:40:56 +0700

    style: fix localization button responsive

--- resources/views/components/locale/EN.blade.php
+++ resources/views/components/locale/EN.blade.php
@@ -1,2 +1,2 @@
-<span class="fw-semibold">English</span>
+<span class="fw-semibold lang-text">English</span>
 <img src="https://flagcdn.com/20x15/gb.png" width="20" height="15" alt="United Kingdom">
--- resources/views/components/locale/ID.blade.php
+++ resources/views/components/locale/ID.blade.php
@@ -1,2 +1,2 @@
-<span class="fw-semibold">Bahasa Indonesia</span>
+<span class="fw-semibold lang-text">Bahasa Indonesia</span>
 <img src="https://flagcdn.com/20x15/id.png" width="20" height="15" alt="Indonesia">
